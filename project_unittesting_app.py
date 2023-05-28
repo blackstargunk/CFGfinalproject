4aa65bb155b65be0e6cc7998c37b897d2d6616e8
Nalani St Louis <102010820+Laniiilou@users.noreply.github.com>
2023-05-28 14:04:12 +0100

    Update project_unittesting_app.py
    added comments for each tested function

--- project_unittesting_app.py
+++ project_unittesting_app.py
@@ -7,12 +7,14 @@ from app import app
 
 class AppTestCase(TestCase):
     def create_app(self):
+        #CSRF protection disabled for the sake of testing for simplification purposes and ease of testing test cases.
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False 
         return app
 
     @patch('app.homepage')
     def test_homepage(self, mock_homepage):
+        #Testing homepage function successfully called - tests the correct response occurs and the right template (index) is used, through mocking the function.
         mock_homepage.return_value = 'Mocked_homepage_response'
         response = self.client.get('/')
         self.assert200(response)
@@ -21,6 +23,7 @@ class AppTestCase(TestCase):
 
     @patch('app.search')
     def test_search(self, mock_search):
+        #Testing search function successfully called - tests the correct response occurs and the right template (search_radio) is used, through mocking the function.
         mock_search.return_value = 'Mocked_search_response'
         response = self.client.get('/search')
         self.assert200(response)
@@ -29,6 +32,7 @@ class AppTestCase(TestCase):
 
     @patch('app.playlists')
     def test_playlists(self, mock_playlists):
+        #Testing playlists funtion successfully called - tests username is working as it should, also through mocking the function.
         mock_playlists.return_value = 'Mocked_playlists_response'
         response = self.client.post('/savedplaylists', data={'username': 'test_user'})
         self.assert200(response)
@@ -38,6 +42,7 @@ class AppTestCase(TestCase):
 
     @patch('app.results')
     def test_results(self, mock_results):
+        #Testing results funtion successfully called - tests results is working as it should, also through mocking the function.
         mock_results.return_value = 'Mocked_results_response'
         response = self.client.post('/results', data={'start_year': '2000', 'end_year': '2020', 'bechdel_rating': 'pass'})
         self.assert200(response)
@@ -47,6 +52,7 @@ class AppTestCase(TestCase):
 
     @patch('app.login')
     def test_login(self, mock_login):
+        #Testing login funtion successfully called
         mock_login.return_value = 'Mocked_login_response'
         response = self.client.get('/login')
         self.assert200(response)
